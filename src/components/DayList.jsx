597b3e4e6b1e144854800f76a737ac2be91ebeab
JC Flores <tothenextcode@gmail.com>
2022-05-17 16:11:33 -0700

    Add DayList to main application

---
+++ src/components/DayList.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import DayListItem from "./DayListItem";
+
+export default function DayList(props) {
+  const dayListItems = props.days.map(day => 
+    <DayListItem 
+      key={day.id}
+      name={day.name}
+      spots={day.spots}
+      setDay={props.setDay}
+    />);
+
+  return (
+    <ul>
+      {dayListItems}
+    </ul>
+  );
+};
