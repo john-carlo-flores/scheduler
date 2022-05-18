64ca93b9c3380ce7139e0baae8c505ce0d021e71
JC Flores <tothenextcode@gmail.com>
2022-05-18 13:21:05 -0700

    Refactor InterviewerList to handle state function logic instead of its child

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -6,10 +6,9 @@ export default function InterviewerList(props) {
   const interviewerListItems = props.interviewers.map(interviewer => 
     <InterviewerListItem 
       key={interviewer.id}
-      id={interviewer.id}
       name={interviewer.name}
       avatar={interviewer.avatar}
-      setInterviewer={props.setInterviewer}
+      setInterviewer={() => props.setInterviewer(interviewer.id)}
       selected={props.interviewer===interviewer.id}
     />
   );
--- src/components/InterviewerListItem.jsx
+++ src/components/InterviewerListItem.jsx
@@ -3,19 +3,19 @@ import classNames from "classnames";
 import "components/InterviewerListItem.scss"
 
 export default function InterviewerListItem(props) {
-  const { id, avatar, name, setInterviewer, selected } = props;
+  const { avatar, name, setInterviewer, selected } = props;
   const listClasses = classNames("interviewers__item", {
     'interviewers__item--selected': selected
   });
 
   return (
-    <li onClick={() => setInterviewer(id)} className={listClasses}>
+    <li onClick={setInterviewer} className={listClasses}>
       <img
         className="interviewers__item-image"
         src={avatar}
         alt={name}
       />
-      {selected ? name : ''}
+      {selected && name}
     </li>
   );
 };
