578dfe5159bd270d269239435852d70f8e9f0eb2
JC Flores <tothenextcode@gmail.com>
2022-05-19 16:01:55 -0700

    Install axios and fix proxy in package.json. Implement axios GET /api/days and store in state

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,25 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = {
   "1": {
@@ -60,14 +43,22 @@ const appointments = {
   }
 };
 
-export default function Application(props) {
+export default function Application() {
   const [ day, setDay ] = useState("Monday");
+  const [ days, setDays ] = useState([]);
   const appointmentList = Object.values(appointments).map(appointment => 
     <Appointment
       key={appointment.id}
       {...appointment}
     />  
   )
+
+  useEffect(() => {
+    axios.get('/api/days')
+      .then(response => {
+        setDays([...response.data]);
+      })
+  }, [])
 
   return (
     <main className="layout">
