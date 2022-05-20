245689216219fda88023f8ec4825e79f16c02e52
JC Flores <tothenextcode@gmail.com>
2022-05-19 17:59:42 -0700

    Import and use getAppointmentsForDay

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,45 +3,8 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
-
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
+import "helpers/selectors";
+import { getAppointmentsForDay } from "helpers/selectors";
 
 export default function Application() {
   const [ state, setState ] = useState({
@@ -51,19 +14,29 @@ export default function Application() {
   });
 
   const setDay = day => setState({ ...state, day });
-  const setDays = days => setState(prev => ({ ...prev, days }));
 
-  const appointmentList = Object.values(state.appointments).map(appointment => 
+  useEffect(() => {
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+    ]).then((all) => {
+      console.log(all);
+
+      setState(prev => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data
+      }));
+    });
+  }, []);
+
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const appointmentList = dailyAppointments.map(appointment => 
     <Appointment
       key={appointment.id}
       {...appointment}
     />  
   );
-
-  useEffect(() => {
-    axios.get('/api/days')
-      .then(response => setDays(response.data))
-  }, []);
 
   return (
     <main className="layout">
