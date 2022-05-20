18365aff4556af94f73dc5b4db7a3be42fbcdfbc
JC Flores <tothenextcode@gmail.com>
2022-05-19 20:12:52 -0700

    Implement getInterview helper function with tests and modified state object to accept interviewers

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,14 +3,14 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
-import "helpers/selectors";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application() {
   const [ state, setState ] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   const setDay = day => setState({ ...state, day });
@@ -19,24 +19,29 @@ export default function Application() {
     Promise.all([
       axios.get('/api/days'),
       axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
     ]).then((all) => {
       console.log(all);
 
       setState(prev => ({
         ...prev,
         days: all[0].data,
-        appointments: all[1].data
+        appointments: all[1].data,
+        interviewers: all[2].data
       }));
     });
   }, []);
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
-  const appointmentList = dailyAppointments.map(appointment => 
-    <Appointment
+  const appointmentList = dailyAppointments.map(appointment => {
+    const interview = getInterview(state, appointment.interview);
+
+    return (<Appointment
       key={appointment.id}
       {...appointment}
-    />  
-  );
+      interview={interview}
+    />);
+  });
 
   return (
     <main className="layout">
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,9 +1,18 @@
 export const getAppointmentsForDay = (state, day) => {
   if (state.days.length === 0) return [];
 
-  const appointmentList = state.days.filter(d => d.name === day)[0];
+  const appointmentList = state.days.find(d => d.name === day);
   if (!appointmentList) return [];
 
   const filteredAppointments = Object.values(state.appointments).filter(appointment => appointmentList.appointments.includes(appointment.id));
   return filteredAppointments;
+};
+
+export const getInterview = (state, interview) => {
+  if (!interview) return null;
+
+  const interviewer = state.interviewers[interview.interviewer];
+  if (!interviewer) return null;
+
+  return { ...interview, interviewer};
 };
