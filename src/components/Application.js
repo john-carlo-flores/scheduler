341bccf1f0928968df5912df9d3399103358a3f8
JC Flores <tothenextcode@gmail.com>
2022-05-19 22:17:25 -0700

    Install react-test-library, dependencies and implement useVisualMode

--- src/components/Application.js
+++ src/components/Application.js
@@ -21,8 +21,6 @@ export default function Application() {
       axios.get('/api/appointments'),
       axios.get('/api/interviewers')
     ]).then((all) => {
-      console.log(all);
-
       setState(prev => ({
         ...prev,
         days: all[0].data,
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export const useVisualMode = (initial) => {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = ((newMode, replace = false) => {
+    setHistory(prev => replace ? [...prev].splice(-1, 1, newMode) : [...prev, newMode]);
+    setMode(newMode);
+  });
+
+  const back = (() => {
+    if (history.length > 1) {
+      setMode(history[history.length - 2]);
+      
+      setHistory(prev => {
+        return [...prev].slice(0, -1);
+      });
+    }
+  });
+
+  return { mode, transition, back };
+};
